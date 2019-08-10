562c3eab0cb97e167cb106d1e65cb8e8c6285903
shaycapehart <shay.capehart@gmail.com>
2019-08-10 01:13:34 -0700

    removed unnecessary comment block

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,3 @@
-
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//   response.json().then((data) => {
-//     console.log(data)
-//   })
-// })
-
-
 const weatherForm = document.querySelector('form')
 const addressInput = document.getElementById('autocomplete_search')
 
