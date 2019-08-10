0a4f2524ddcb7160ba99529aff6f2f4f86e3da7c
shaycapehart <shay.capehart@gmail.com>
2019-08-10 03:24:59 -0700

    changed from Places API to Mapbox

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,20 +1,20 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-  const url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodeURIComponent(address) + '&key=AIzaSyC8Es9ghSwn5wbgoRqRiR6CeniIUF4xN1E'
+  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYnJpY2tzOTYiLCJhIjoiY2p6NHRqejFlMGh3ZDNnbWV3Z2x6eWZ6ZSJ9.6BsH1CmcvlNKlrWX88PLHA&limit=1'
   request({ url, json: true }, (error, {body}) => {
     if (error) {
       callback('Unable to connect to google geocoding service!', undefined)
     }
-    else if (body.results.length === 0) {
+    else if (body.features.length === 0) {
       callback('Unable to find location. Try another search', undefined)
     }
     else {
-      console.log(body.results)
+      console.log(body.features[0])
       const data = {
-        latitude: body.results[0].geometry.location.lat,
-        longitude: body.results[0].geometry.location.lng,
-        location: body.results[0].formatted_address
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name
       }
       callback(undefined, data)
     }
